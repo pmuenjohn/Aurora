10464e07b958f31553ec1e508d7e3444a6f57113
pmuenjohn <muenjohnt@gmail.com>
2022-10-24 20:57:24 +1100

    Fix prefabs, Bake lighting on Kong's levels, Add change scene triggers.

---
+++ Assets/Scripts/ChangeSceneTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneTrigger : MonoBehaviour
+{
+    public string nextSceneName;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
